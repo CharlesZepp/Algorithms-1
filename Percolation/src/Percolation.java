5c9cac193f4ab01de2e907952e61b5a569619a6c
CharlesZepp <zepp1@umbc.edu>
2017-08-03 16:25:47 -0400

    init commit

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,166 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/**
+ * 
+ * @author charl
+ *
+ */
+
+public class Percolation {
+	
+	private final int vT;                          //virtual top
+	private final int vB;                          //virtual bottom
+	private final WeightedQuickUnionUF grid;               
+	private final WeightedQuickUnionUF backwash;   //used to fix backwash problem
+	private boolean[]site;
+	private int n;
+	
+	/**
+	 * create n-by-n grid, with all sites blocked
+	 * @param n
+	 * @throws IllegalArgumentException
+	 */
+	public Percolation(int n)
+	{
+		if (n <= 0)
+		{
+			throw new IllegalArgumentException("n must be greater than zero");
+		}
+		else
+		{
+		    this.n = n;
+            this.vT = 0;
+            this.vB = n*n+1;
+            grid = new WeightedQuickUnionUF(n*n + 2); 
+            backwash = new WeightedQuickUnionUF(n*n+ 1);
+            site = new boolean[n*n];
+		}
+	} 
+	
+	/**
+	 * Validates parameters and opens site.
+	 * Checks surrounding sites and if they are open the connected them through union().
+	 * @param row
+	 * @param col
+	 */
+	public void open(int row, int col)
+	{
+	    validate(row, col);
+	    
+	    int index = siteIndex(row, col);
+	    // open site
+	    site[index] = true;
+	    
+	    if (row == 1)//top row
+        { 
+            grid.union(index, vT); 
+            backwash.union(index, vT);
+        }
+        if (row == n)//bottom row
+        {
+            grid.union(index, vB);
+        }
+        
+        if (row > 1 && isOpen(row-1, col))//left of site
+        {
+            grid.union(index, siteIndex(row-1, col));
+            backwash.union(index, siteIndex(row-1, col));
+        }
+        
+        if (row < n && isOpen(row+1, col))//right of
+        {
+            grid.union(index, siteIndex(row+1, col));
+            backwash.union(index, siteIndex(row+1, col));
+        }
+        
+        if (col > 1 && isOpen(row, col-1))//above 
+        {
+            grid.union(index, siteIndex(row, col-1));
+            backwash.union(index, siteIndex(row, col-1));
+        }
+        
+        if (col < n && isOpen(row, col+1))//below
+        {
+            grid.union(index, siteIndex(row, col+1));
+            backwash.union(index, siteIndex(row, col+1));
+        }
+	}
+	
+	/**
+	 * validates parameters and checks whether site is open or not
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	public boolean isOpen(int row, int col)
+	{
+		validate(row, col);
+		return site[siteIndex(row, col)];
+	}
+	
+	/**
+	 * Validates parameters and checks whether site is either directly connected to virtual top(vT)
+	 * or connected through other open sites
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	public boolean isFull(int row, int col)
+	{
+		validate(row, col);
+		return  site[siteIndex(row, col)] && backwash.connected(vT, siteIndex(row, col));
+	}
+	
+	/**
+	 * Returns the number of open sites
+	 * @return
+	 */
+	public int numberOfOpenSites()
+	{
+	    int open = 0;
+	    for (int i = 0; i < site.length; i++)
+	    {
+	        if (site[i])
+	        {
+	            open++;
+	        }
+	    }
+		return open;
+	}
+	
+	/**
+	 * Checks whether the Virtual top and Virtual Bottom are connected through opened sites
+	 * Meaning that the grid percolates
+	 * @return
+	 */
+	public boolean percolates()
+	{
+		return grid.connected(vT, vB);
+	}
+	
+	//Added Private Methods
+	
+	/**
+	 * Validates parameters then changes row and col into 1D arr[] indexes (0-n)
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	private int siteIndex(int row, int col)
+	{
+		validate(row, col);
+		return ((n * (row-1) + col)-1);
+	}
+
+	/**
+	 * Validates row and col parameters to makes sure they are within specified grid (1 through N)
+	 * @param row
+	 * @param col
+	 * @throws IllegalArgumentException
+	 */
+	private void validate(int row, int col)
+	{
+		if ((row-1 < 0 || row-1 > n) || (col-1 < 0 || col-1 > n))
+			throw new IllegalArgumentException("Out of bounds");
+	}
+}
---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,104 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+/**
+ * 
+ * @author charl
+ *
+ */
+
+public class PercolationStats {
+	
+	private int trials;
+	private double[] results;
+	
+	/**
+	 * perform trials independent experiments on an n-by-n grid
+	 * @param n
+	 * @param trials
+	 * @throws 
+	 */
+	public PercolationStats(int n, int trials)
+	{	
+		if (n <= 0 || trials <= 0)
+		{
+			throw new IllegalArgumentException("Grid size and number of trials must be greater than '0'");
+		}
+		else
+		{
+			this.trials = trials;
+			results = new double[trials];
+			
+			for (int i = 0; i < results.length; i++)
+			{
+				Percolation p = new Percolation(n);
+				int x = 0;
+				while (!p.percolates())
+				{
+				    int row = StdRandom.uniform(1, n+1);
+				    int col = StdRandom.uniform(1, n+1);
+				    
+				    if (!p.isOpen(row, col))
+				    {
+				        p.open(row, col);
+				        x++;
+				    }
+				}
+				results[i] = (double) x /(double) (n*n); //result
+			}
+		}	
+	}
+	
+	/**
+	 * sample mean of percolation threshold
+	 * @return
+	 */
+	public double mean()
+	{
+		return StdStats.mean(results);
+	}
+	
+	/**
+	 * sample standard deviation of percolation threshold
+	 * @return
+	 */
+	public double stddev()
+	{
+		return StdStats.stddev(results);
+	}
+	
+	/**
+	 * low  endpoint of 95% confidence interval
+	 * @return
+	 */
+	public double confidenceLo()
+	{
+		return mean() - ((1.96*stddev())/Math.sqrt(trials));
+	}
+	
+	/**
+	 * high endpoint of 95% confidence interval
+	 * @return
+	 */
+	public double confidenceHi()
+	{
+		return mean() + ((1.96*stddev())/Math.sqrt(trials));
+	}
+	
+	/**
+	 * MAIN (test client)
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		int n = Integer.parseInt(args[0]);
+		int trials = Integer.parseInt(args[1]);
+		
+		PercolationStats stats = new PercolationStats(n, trials);
+		
+		System.out.println("---Percolation Stats---------------------------------------------------");
+		System.out.println("Mean                     =" + stats.mean());
+		System.out.println("Std                      =" + stats.stddev());
+		System.out.println("95% Confidence Interval  =[ "+ stats.confidenceLo() + ", " + stats.confidenceHi() + " ]");
+	}
+}
